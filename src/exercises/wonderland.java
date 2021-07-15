156ebba3a263b0940ce2a15949af0be762f5fffb
msajsnow <84166958+msajsnow@users.noreply.github.com>
2021-07-14 21:04:50 -0500

    added area class

---
+++ src/exercises/wonderland.java
@@ -0,0 +1,33 @@
+package exercises;
+import java.util.Scanner;
+
+public class wonderland {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String firstSentence = ("Alice was beginning to get very tired of sitting" +
+                "by her sister on the bank, and of having nothing to do: once or " +
+                "twice she had peeped into the book her sister was reading, but it had" +
+                "no pictures or conversations in it, " +
+                "'and what is the use of a book,' thought Alice 'without pictures or conversation?'");
+
+//        Integer index = firstSentence.indexOf(searchTerm);
+//        Integer length = searchTerm.length();
+//
+//        System.out.println("Your search term first appears at index " + index + ". Your term is " + length + " characters long.");
+//
+//        String modifiedSentence = firstSentence.replace(searchTerm, "");
+//        System.out.println(modifiedSentence);
+
+
+    }
+}
+//    Integer index = firstSentence.indexOf(searchTerm);
+//    Integer length = searchTerm.length();
+//System.out.println("Your search term first appears at index " + index + ". Your term is " + length + " characters long.");
+//        String modifiedSentence = firstSentence.replace(searchTerm, "");
+//        System.out.println(modifiedSentence);
+//
+//    Scanner input = new Scanner(System.in);
+//        System.out.println("Hello, what is your name: ");
+//                String name = input.nextLine();
+//                System.out.println("Hello " + name);
